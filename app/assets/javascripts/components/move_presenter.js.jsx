1b00bf06c82fe137882a3ed05a4da5c181187db5
Amine_Zerradi <dasileker@gmail.com>
2021-10-22 17:35:02 +0100

    add components

---
+++ app/assets/javascripts/components/move_presenter.js.jsx
@@ -0,0 +1,43 @@
+MovePresenter = React.createClass({
+  getInitialState: function () {
+    return ({ moves: this.props.moves })
+  },
+
+  updateMoves: function (data) {
+    alert(data);
+  },
+
+  componentDidMount: function () {
+    this.setupCable();
+  },
+
+  setupCable: function () {
+    App.move = App.cable.subscriptions.create("MoveChannel",
+      {
+        connected: function () {
+        },
+
+        disconnected: function () {
+        },
+
+        received: function (data) {
+          this.updateMoves(data);
+          alert(data)
+        },
+
+        selectTile: function(data) {
+          this.perform('select_tile', { tile: data})
+        },
+
+        updateMoves: this.updateMoves
+      });
+  },
+
+  render: function () {
+    return (
+      <div id='moves'>
+        <MoveList moves={ this.props.moves} />
+      </div>
+    )
+  }
+})
